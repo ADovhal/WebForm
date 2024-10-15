c06a868f7a7d5d8e1f9f671c14d400fdddc8372b
Andrii Dovhal <dovhaland@gmail.com>
2024-10-15 12:58:03 +0200

    Removed old version Server part

--- server-application/src/main/java/com/dovhal/serverapplication/repos/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dovhal.serverapplication.repos;
-
-import com.dovhal.serverapplication.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-interface UserRepository extends JpaRepository<User, Integer> {
-
-}
